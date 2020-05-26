2b136b1f43662cbf1e4f95637930d8b1f0469d04
summersted <romathedutch@gmail.com>
2020-05-26 05:35:29 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/*
+$(window).scroll(function(){
+    if ($(window).scrollTop() > 200) {
+        $('.header').addClass('scroll');
+        $('.scroll').removeClass('header')
+    }
+    else {
+        $('.header').removeClass('scroll')
+        $('.scroll').addClass('header');
+    }
+});
